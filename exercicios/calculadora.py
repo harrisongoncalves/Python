9420deea4b8ef195f4aac57d5d615e5f725ac2bf
H3yF3n1x <brzeirogamer@gmail.com>
2023-02-02 15:17:52 -0300

    Calculadora de Operações Básicas
    Esta é uma calculadora de operações básicas (adição, subtração, multiplicação e divisão) também foi feita para exercício de algum curso.

---
+++ exercicios/calculadora.py
@@ -0,0 +1,32 @@
+import time
+
+def operacao(n1, n2):
+    if sinal == "+" or sinal == "soma":
+        resultado = n1 + n2
+        return resultado
+    elif sinal == "-" or sinal == "subtracao":
+        resultado = n1 - n2
+        return resultado
+    elif sinal == "*" or sinal == "multiplicação":
+        resultado = (n1 * n2)
+        return resultado
+    elif sinal == "/" or sinal == "divisão":
+        resultado = n1 / n2
+        return resultado
+
+while True:
+    try:
+        n1 = float(input("Digite o primeiro número: "))
+        sinal = input("Agora digite o sinal, dentre as seguintes opções: soma: + subtracao: - multiplicação: * divisão: / : ")
+        n2 = float(input("Digite o segundo número: "))
+        
+    except ValueError:
+        print("Opção inválida.")
+        time.sleep(1.25)
+    else:
+        break
+
+sinal = sinal.strip()
+sinal = sinal.lower()
+print(operacao(n1, n2))
+
